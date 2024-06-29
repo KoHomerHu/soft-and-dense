Fix bugs in square_square_energy_loss computation

decoder.py
@@ -64,8 +64,6 @@ class Decoder(nn.Module):
     """
     def forward(self, mapping: List[Dict], batch_size, lane_states_batch: List[Tensor], 
                 inputs: Tensor, inputs_lengths: List[int], hidden_states: Tensor, device):
-        # labels = utils.get_from_mapping(mapping, 'labels')
-        # labels_is_valid = utils.get_from_mapping(mapping, 'labels_is_valid')
         loss = torch.zeros(batch_size, device=device)
 
         dense_goal_scores_lst, dense_goals_lst = [], []
@@ -85,11 +83,11 @@ class Decoder(nn.Module):
             for i in range(batch_size):
                 loss[i] += utils.square_square_energy_loss(
                     dense_goals_lst[i],
-                    F.softplus(dense_goal_scores_lst[i]), 
+                    dense_goal_scores_lst[i], 
                     mapping[i]
                 )
 
-        dense_goal_scores_numpy = [F.softplus(dense_goal_scores).detach().cpu().numpy() for dense_goal_scores in dense_goal_scores_lst]
+        dense_goal_scores_numpy = [dense_goal_scores.detach().cpu().numpy() for dense_goal_scores in dense_goal_scores_lst]
         return loss.mean(), dense_goal_scores_numpy, dense_goals_lst
 
     """
@@ -166,7 +164,7 @@ class Decoder(nn.Module):
             goals_2D_hidden_attention_with_lane
         ]
         scores = self.goals_2D_decoder(torch.cat(li, dim=-1)).squeeze(-1)
-        # scores = F.softmax(scores, dim=-1)
+        scores = F.softplus(scores)
 
         return scores
     
@@ -175,10 +173,9 @@ class Decoder(nn.Module):
     """
     def get_dense_goal_scores(self, i, sparse_goals, mapping, device, scores, get_scores_inputs, k=150):
         # Sample dense goals from top K sparse goals.
-        _, topk_ids = torch.topk(scores, k=min(k, len(scores)))
+        _, topk_ids = torch.topk(-scores, k=min(k, len(scores))) # energies for top choice of goals are small
         dense_goals = utils.get_neighbour_points(sparse_goals[topk_ids.cpu()], topk_ids=topk_ids, mapping=mapping[i], neighbour_dis=2)
         dense_goals = utils.get_points_remove_repeated(dense_goals, decimal=0) # remove repeated points
-        # dense_goals = torch.tensor(dense_goals, device=device, dtype=torch.float)
         # include the sparse goals
         dense_goals = torch.cat(
             [

utils.py
@@ -466,17 +466,20 @@ def get_dense_goal_targets(dense_goals: np.ndarray, mapping: List[Dict], T=50.0,
     return dense_goal_targets
 
 
-def square_square_energy_loss(dense_goals, dense_goal_scores, mapping, m=5):
+"""
+Taken out of square_square_energy_loss to be used for multiprocessing (deprecated).
+"""
+def get_sse_prep(dense_goals, dense_goal_scores, mapping, m=10):
     ground_truth_goal = mapping['labels'][-1]
     compute_traj = mapping['quadratic_path'] is not None
 
-    target_energy = dense_goal_scores[np.argmin(get_dis_batch(dense_goals, ground_truth_goal))]
+    target_energy_idx = np.argmin(get_dis_batch(dense_goals, ground_truth_goal))
 
-    mo_idx, mo_dist = None, float('inf') # index and distance of the most offensive target
+    mo_idx, mo_score = None, float('inf') # index and distance of the most offensive target
 
     for i, goal in enumerate(dense_goals):
         goal_dist = get_dis_p2p(goal, ground_truth_goal) # distance between the goal and the ground truth goal
-        if goal_dist <= 10:
+        if goal_dist <= m + 0.5:
             # Compute goal and reference path attraction
             if compute_traj:
                 _, point_hat = inv_proj(goal, mapping['quadratic_path'])
@@ -484,16 +487,21 @@ def square_square_energy_loss(dense_goals, dense_goal_scores, mapping, m=5):
             else:
                 traj_dist = 0.0
             dist = goal_dist + 2 * traj_dist
-            if dist < mo_dist and dist >= m:
-                mo_dist = dist
+            score = dense_goal_scores[i].item()
+            if dist >= m and score < mo_score:
+                mo_score = score
                 mo_idx = i
 
-    if mo_idx is not None:
-        most_offsensive_target_energy = dense_goal_scores[mo_idx]
-    else:
-        most_offsensive_target_energy = 0.0
+    return target_energy_idx, mo_idx
 
-    return target_energy ** 2 + max(0, m - most_offsensive_target_energy) ** 2
+
+def square_square_energy_loss(dense_goals, dense_goal_scores, mapping, m=10):
+    target_energy_idx, mo_idx = get_sse_prep(dense_goals, dense_goal_scores, mapping, m)
+
+    target_energy = dense_goal_scores[target_energy_idx]
+    mo_target_energy = dense_goal_scores[mo_idx] if mo_idx is not None else 0.0
+
+    return target_energy ** 2 + max(0, m - mo_target_energy) ** 2
 
 
 def get_dense_goal_targets_one_hot(dense_goals: np.ndarray, mapping: List[Dict]):